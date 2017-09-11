8003fe73e3b7ccee83a9be6728629cbcedb9b08c
tompawlo <pawlowski.tomq@gmail.com>
2017-09-11 22:10:02 +0200

    obsluga streamy + face - poprawki

--- PKM2/ALGORYTMY/twarz.py
+++ PKM2/ALGORYTMY/twarz.py
@@ -1,24 +1,21 @@
 import cv2
-import time
-import requests
-import threading
-from threading import Thread, Event, ThreadError
 
+## xml'ka dzieki ktorej, mozemy wykrywac face'y
 faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
 def twarz(frame):
-
+    # ramka w odcieniach szarosci
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
+    #faces to lista punktow (x, y, w, h)
     faces = faceCascade.detectMultiScale(
         gray,
+        # parametry dobrane tak,by wykrywanie dzialalo najlepiej
         scaleFactor=1.15,
         minNeighbors=5,
         minSize=(20, 20),
         flags=cv2.CASCADE_SCALE_IMAGE
     )
-
-        # Draw a rectangle around the faces
+        # rysowanie prostokata wokol twarzy
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 255), 2)
-
     return frame
