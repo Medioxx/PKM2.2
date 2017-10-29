dfda420caa300a0919854770cf4a5ab060c32873
Szonek - Szymon Marcinkowski <szymonpl17@gmail.com>
2017-10-29 04:16:05 +0100

    changes

--- PKM2/INNE/train_detector/wrappers.py
+++ PKM2/INNE/train_detector/wrappers.py
@@ -1,4 +1,6 @@
 import cv2
+from imutils import contours
+
 from color_labeler import ColorLabeler
 import imutils
 import copy
@@ -11,12 +13,14 @@ class ImageWrapper:
         self.ratio = ratio
         self.height, self.width = self.image.shape[:2]
         self.resized = cv2.resize(self.image, (int(self.height * ratio), int(self.width * ratio)))
-        self.blurred = cv2.GaussianBlur(self.image, (5, 5), 0)  # TODO CHANGE FROM SELF.IMAGE TO SELF.RESIZED LATER !!!
+        self.blurred = cv2.GaussianBlur(self.image, (5, 5), 0)
+        self.hsv = cv2.cvtColor(cv2.GaussianBlur(self.image, (11, 11), 0), cv2.COLOR_BGR2HSV)
         self.gray = cv2.cvtColor(self.blurred, cv2.COLOR_BGR2GRAY)
         self.edged = cv2.Canny(self.blurred, 50, 150)
         self.lab = cv2.cvtColor(self.blurred, cv2.COLOR_BGR2LAB)
         self.thresh = cv2.threshold(self.gray, 60, 255, cv2.THRESH_BINARY)[1]
         self.cnts = None
+
 
     def __contours(self, image):
         self.cnts = cv2.findContours(image.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -40,6 +44,7 @@ class ContourWrapper:
         self.M = cv2.moments(self.contour)
         (self.x, self.y, self.w, self.h) = cv2.boundingRect(self.approx)
         self.bounding_rect = (self.x, self.y, self.w, self.h)
+        ((self.x_mnc, self.y_mnc), self.radius) = cv2.minEnclosingCircle(contour)
         self.area = cv2.contourArea(self.contour)
         # cX and cY are center of mass of contour
         self.cX, self.cY = self.__get_cx_cy()
