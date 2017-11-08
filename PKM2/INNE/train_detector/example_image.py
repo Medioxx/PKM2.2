9c679626674c9f6b5ce7d0023fd3c41d92a77fe5
Szonek - Szymon Marcinkowski <szymonpl17@gmail.com>
2017-11-08 20:54:14 +0100

    python3.6

--- PKM2/INNE/train_detector/example_image.py
+++ PKM2/INNE/train_detector/example_image.py
@@ -1,12 +1,18 @@
 from shape_detector import ShapeDetector
 <<<<<<< HEAD
+<<<<<<< HEAD
 from hsv_detection import HSV_Detector
+=======
+>>>>>>> e1c56d5... python3.6
 import cv2
 import color_labeler
 
 def image():
     frame = cv2.imread("../shapes/test2.jpg")
+<<<<<<< HEAD
 
+=======
+>>>>>>> e1c56d5... python3.6
     shape = ShapeDetector(frame)
     cv2.imshow('imuout', shape.IW.output_image)
 
@@ -34,6 +40,7 @@ def image():
 
 def video():
 <<<<<<< HEAD
+<<<<<<< HEAD
 
     cap = cv2.VideoCapture('../shapes/biale_przejazd_z_znacznikami.avi')
 
@@ -42,6 +49,9 @@ def video():
     frame_width = cap.get(3)
     frame_height = cap.get(4)
 >>>>>>> ea1d704... inital algorithm
+=======
+    cap = cv2.VideoCapture('../shapes/video1.mp4')
+>>>>>>> e1c56d5... python3.6
     while cap.isOpened():
         ret, frame = cap.read()
 
@@ -57,8 +67,12 @@ def video():
         #cv2.imshow('framIN', shape.IW.image)
         cv2.imshow('frameOUT', shape.IW.output_image)
         cv2.imshow('frameedged', shape.IW.edged)
+<<<<<<< HEAD
         print "frame" + str(cap.get(cv2.cv.CV_CAP_PROP_POS_FRAMES))
 >>>>>>> ea1d704... inital algorithm
+=======
+        #print("frame" + str(cap.get(cv2.cv.CV_CAP_PROP_POS_FRAMES)))
+>>>>>>> e1c56d5... python3.6
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     cap.release()
@@ -72,6 +86,9 @@ def web_cam():
         ret, frame = cam.read()
         shape = ShapeDetector(frame)
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> e1c56d5... python3.6
 
         cv2.imshow('original webcam', shape.IW.image)
         cv2.imshow('processed image', shape.IW.output_image)
@@ -87,6 +104,7 @@ def web_cam():
 
 
 def main():
+<<<<<<< HEAD
     #image()
 <<<<<<< HEAD
     video()
@@ -95,6 +113,11 @@ def main():
     #video()
     web_cam()
 >>>>>>> ea1d704... inital algorithm
+=======
+    image()
+    #video()
+   #web_cam()
+>>>>>>> e1c56d5... python3.6
     pass
 
 
--- PKM2/INNE/train_detector/shape_detector.py
+++ PKM2/INNE/train_detector/shape_detector.py
@@ -13,10 +13,14 @@ triangle_str = "triangle"
 
 class Shape:
 <<<<<<< HEAD
+<<<<<<< HEAD
     def __init__(self, type="", color="", area=0, center_x=0, center_y=0, x=0, y=0, w=0, h=0):
 =======
     def __init__(self, type="", color="", area=0, center_x=0, center_y=0, (x, y, w, h)=(0, 0, 0, 0)):
 >>>>>>> ea1d704... inital algorithm
+=======
+    def __init__(self, type="", color="", area=0, center_x=0, center_y=0, x=0, y=0, w=0, h=0):
+>>>>>>> e1c56d5... python3.6
         self.type = type
         self.color = color
         self.area = area
@@ -49,10 +53,14 @@ class Shape:
         pass
 
 <<<<<<< HEAD
+<<<<<<< HEAD
     def set_size(self, x, y, w, h):
 =======
     def set_size(self, (x, y, w, h)):
 >>>>>>> ea1d704... inital algorithm
+=======
+    def set_size(self, x, y, w, h):
+>>>>>>> e1c56d5... python3.6
         (self.x, self.y, self.w, self.h) = (x, y, w, h)
         pass
 
@@ -119,6 +127,7 @@ class ShapeDetector:
                     array_of_contours = self.add_cw_to_similarity_array(array_of_contours, CW)
                     #self.shape.set_color(CW.get_color(self.IW.lab))
                     #if self.shape.is_blue():
+<<<<<<< HEAD
 
             if self.shape.is_triangle():
                 if self.shape.is_area_higer_than(300):
@@ -129,6 +138,22 @@ class ShapeDetector:
         #for cont in array_of_contours:
             #print(str(cont.cX) + ", " + str(cont.cX) + ", " + str(cont.area))
 
+=======
+                    self.detected = True
+                    self.shape.set_center(CW.cX, CW.cY)
+                    self.shape.set_size(CW.x, CW.y, CW.w, CW.h)
+
+                    print(self.shape)
+                    print(CW.CL.mean)
+
+            if len(CW.approx == 3):
+                if 10 <= CW.area <= 100:
+                    cv2.drawContours(self.IW.output_image, [CW.approx], -1, (0, 255, 255), 4)
+                    #graphics_utils.draw_contour(self.IW.output_image, CW.approx)
+
+        for cont in array_of_contours:
+            print(str(cont.cX) + ", " + str(cont.cX) + ", " + str(cont.area))
+>>>>>>> e1c56d5... python3.6
 
         #POTEM TU ZMIENIC, NARAZIE TESTOWO W TAKI BRZYDKI SPOSOB
         # shape a jest wiekszy od shape b, czyli b to figury z ktorej bedzie wyciagany kolor
@@ -138,9 +163,15 @@ class ShapeDetector:
             if a is None and b is None:
                 pass
             else:
+<<<<<<< HEAD
                 #print("podejrzane kontury: " + str(a.area) + ", " + str(b.area))
                 #print("cX_1: " + str(a.cX) + ", cY_1: " + str(a.cY))
                 #print("cX_2: " + str(b.cX) + ", cY_2: " + str(b.cY))
+=======
+                print("podejrzane kontury: " + str(a.area) + ", " + str(b.area))
+                print("cX_1: " + str(a.cX) + ", cY_1: " + str(a.cY))
+                print("cX_2: " + str(b.cX) + ", cY_2: " + str(b.cY))
+>>>>>>> e1c56d5... python3.6
                 self.shape.set_center(b.cX, b.cY)
                 self.shape.set_size(b.x, b.y, b.w, b.h)
                 graphics_utils.draw_contour(self.IW.output_image, a.approx)
@@ -180,11 +211,19 @@ class ShapeDetector:
         ratio = 0
         for i in range(0, len(cnts_array)):
             for j in range(0, len(cnts_array)):
+<<<<<<< HEAD
                 if cnts_array[j].area != 0:
                     ratio = cnts_array[i].area / cnts_array[j].area
                     if abs(ratio-expected_ratio) <= err and self.check_similarity_of_two_cw(cnts_array[i], cnts_array[j]):
                         #print("abs ratio" + str(abs(ratio-expected_ratio)))
                         return cnts_array[i], cnts_array[j]
+=======
+                ratio = cnts_array[i].area / cnts_array[j].area
+                print(ratio)
+                if abs(ratio-expected_ratio) <= err and self.check_similarity_of_two_cw(cnts_array[i], cnts_array[j]):
+                    print("abs ratio" + str(abs(ratio-expected_ratio)))
+                    return cnts_array[i], cnts_array[j]
+>>>>>>> e1c56d5... python3.6
         return None, None
 
     def check_similarity_of_two_cw(self, cw_1, cw_2):
