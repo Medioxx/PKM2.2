0f249cea72c1846b0eaf7775dc8d35691f6a485d
dhwid <da_wid@onet.pl>
2017-11-09 20:52:33 +0100

    Kamera z gory (#26)
    * wykrywanie torow
    
    * podzial obrazu na mniejsze obszary, wykrywanie lokalnych maximum na wykresie
    
    * peakdetect
    
    * added kmeans, finding trains

--- PKM2/ALGORYTMY/kamera_z_gory/widok_z_gory.py
+++ PKM2/ALGORYTMY/kamera_z_gory/widok_z_gory.py
@@ -1,90 +1,117 @@
 import cv2
-import numpy as np
 import matplotlib.pyplot as plt
 from peakdetect import *
+from kmeans import *
+import os
 
 
 def group_lines(lines):
-
     for line in lines:
         x = line[0][0]/12.80
         grouped[int(x)] += 1
     return grouped
+  
+def find_rails(lines, clusters ,avg):
+    max, _ = peakdet(group_lines(lines), 20)
 
-def find_rails(lines):
-    max, min = peakdet(group_lines(lines), 10)
+    max = np.asarray(max)
 
-    sum = 0
-    for element in max:
-        sum += element[1]
+    print(np.median(max, axis= 0))
 
-    avg = sum/max.__len__()
+    mu, clusters  = find_centers(max,2)
 
-    new_max = []
-    for element in max:
-        if(element[1] > avg):
-            new_max.append(element)
+    if(clusters[0][0][1] > clusters[1][0][1]):
+        cluster = 0
+    else:
+        cluster = 1
 
-    plt.plot(new_max, "x")
+    for i in range(clusters[cluster].__len__()):
+        plt.plot(clusters[cluster][i][0],clusters[cluster][i][1], "x")
+
     plt.show()
 
-    return sum/max.__len__()
+    return clusters[cluster], avg
+  
+def find_trains(lines, clusters, avg):
+    peaks, _ = peakdet(group_lines(lines), 10)
+    new_peaks = []
+    rails = []
 
-def przeszkody(frame, counter):
-    tory = []
+    max = 1.00;
+    sum = 0.00
+    rail = -1
+    match = 0.00
+
+    for k in range(0, clusters.__len__()):
+        for i in range(0, peaks.__len__()):
+            if(abs(clusters[k][0] - peaks[i][0]) < 3):
+                # peaks[i][1] = cluster[1] - peaks[i][1]
+
+                diff = float(peaks[i][1]) / float(clusters[k][1])
+                sum += diff
+                rails.append(diff)
+
+                if(max > diff):
+                    max = diff
+                    rail = k + 1
+                    new_peaks.append(peaks[i])
+                break;
+
+    for i in range(0, rails.__len__()):
+        if(rails[i] < 0.8):
+            print("Pociag na torze", i+1),
+            print(rails[i]),
+
+    print()
+
+    # plt.plot(max, "x")
+    plt.show()
+
+    return
+
+def przeszkody(frame, counter, rails, clusters, avg):
     subframe=frame
-    empty = True
+
 
     # wykrywanie lini
     edges = cv2.Canny(subframe, 50, 250, apertureSize=3)
     minLineLength = 50
-    numberOfLines = 0
     lines = cv2.HoughLinesP(image=edges, rho=1, theta=np.pi / 180, threshold=70, lines=np.array([]),
                             minLineLength=minLineLength, maxLineGap=5)
-    if lines is None:
-        empty = False
 
     group_lines(lines)
 
-    if (counter == 10):
+    if (counter == 20):
 
-        for element in grouped:
-            if element > 20:
-                tory.append(1)
-
-        print(find_rails(lines))
-
+        if(clusters.__len__() == 0):
+            clusters, avg = find_rails(lines, clusters, avg)
+        else:
+            find_trains(lines, clusters, avg)
 
         counter = 0
         for element in range (0, grouped.__len__()):
             grouped[element] = 0
 
+    for element in rails:
+        cv2.line(subframe, (int(element * 12.8), 0), (int(element * 12.8), 1024), (0, 0, 255), 6,
+                  cv2.LINE_8)
 
-    if (empty):
-        a, b, c = lines.shape
-        for i in range(a):
-            if (abs(lines[i][0][2] - lines[i][0][0]) < 50):
-                cv2.line(subframe, (lines[i][0][0], lines[i][0][1]), (lines[i][0][2], lines[i][0][3]), (0, 0, 255), 6,
-                         cv2.LINE_8)
-            #for j in range(a):
-                # cv2.rectangle(subframe,(lines[i][0][0], lines[i][0][1]), ( lines[i][0][2], lines[i][0][3]), (0, 255, 0), 5 )
-
-    return grouped, counter
+    return grouped, counter, rails, clusters, avg
 
 
-
-video = cv2.VideoCapture('C:\Users\Dawid\Desktop\\filmiki\\test1c.avi')
+path = os.getcwd() + "\streams\\"
+filename = "5.avi"
+video = cv2.VideoCapture(path + filename)
 grouped = [0] * 100
+rails = []
+clusters = []
+avg = 0
 counter = 0
 
 while(video.isOpened()):
     _, frame = video.read()
 
-    num_rows, num_cols = frame.shape[:2]
-    rotation_matrix = cv2.getRotationMatrix2D((num_cols / 2, num_rows / 2), -90, 1)
-   # frame = cv2.warpAffine(frame, rotation_matrix, (num_cols, num_rows))
-
-    grouped, counter = przeszkody(frame, counter)
+    grouped, counter, rails, clusters, avg = przeszkody(frame, counter, rails, clusters, avg)
     counter += 1
 
     cv2.imshow('frame',frame)
