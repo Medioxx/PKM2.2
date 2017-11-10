c3abc5ba47c9ab904e084a118c6a5c2c526dd0ed
adamos581 <adabelni@student.pg.gda.pl>
2017-11-10 11:30:46 +0100

    Add read stream activity (#28)
    * Rename all shit
    
    * Rename all shit
    
    * Refactor package
    
    * add start screen to mobile PKM application
    
    * add simple video feature to application
    
    * work with adding listView containing checkboxes with algorithms
    
    * add setting url activity
    
    * send data to mainActivity
    
    * Create preference screen with one variable
    
    * Add library to read stream in Mjpeg
    
    * Add library to read stream in Mjpeg
    
    * Enable use java8 without jack
    
    * Add reading stream
    
    * Add stream screen to xml and remove unnecessary classes.
    
    * Finally is working

---
+++ PKM2/MOBILE_APP/app/src/main/java/pl/eti/pg/pkm/pkm/DataPassingMessages.java
@@ -0,0 +1,13 @@
+package pl.eti.pg.pkm.pkm;
+
+/**
+ * Created by adambelniak on 28.10.2017.
+ */
+
+public class DataPassingMessages {
+    public static final String URLActivityAddress = "pl.eti.pg.pkm.pkm.ADDRESS";
+    public static final String URLActivityPort = "pl.eti.pg.pkm.pkm.PORT";
+
+
+}
+
--- PKM2/MOBILE_APP/app/src/main/java/pl/eti/pg/pkm/pkm/MainActivity.java
+++ PKM2/MOBILE_APP/app/src/main/java/pl/eti/pg/pkm/pkm/MainActivity.java
@@ -1,13 +1,25 @@
 package pl.eti.pg.pkm.pkm;
 
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.VideoView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -15,11 +27,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private VideoView videoView;
     private ImageButton btnPlayPause;
     private String videoURL = "https://archive.org/download/ksnn_compilation_master_the_internet/ksnn_compilation_master_the_internet_512kb.mp4";
+    private MyCustomAdapter dataAdapter = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        displayListView();
 
         videoView = (VideoView)findViewById(R.id.videoView);
         btnPlayPause = (ImageButton) findViewById(R.id.btn_play_pause);
@@ -64,4 +79,63 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         });
     }
+
+    private void displayListView() {
+
+        List<Algorithm> algorithmsList = new ArrayList<Algorithm>();
+
+        Algorithm algorithm = new Algorithm("Zajezdnia", false);
+        algorithmsList.add(algorithm);
+        algorithm = new Algorithm("Przeszkody", false);
+        algorithmsList.add(algorithm);
+        algorithm = new Algorithm("Twarz", false);
+        algorithmsList.add(algorithm);
+        algorithm = new Algorithm("Banan", false);
+        algorithmsList.add(algorithm);
+        algorithm = new Algorithm("Perony", false);
+        algorithmsList.add(algorithm);
+        algorithm = new Algorithm("Ruch ręką", false);
+        algorithmsList.add(algorithm);
+        algorithm = new Algorithm("Ruch pociągu", false);
+        algorithmsList.add(algorithm);
+
+//        dataAdapter = new MyCustomAdapter(this, R.layout.activity_listview, algorithmsList);
+        ListView listView = (ListView) findViewById(R.id.listView1);
+
+        listView.setAdapter(dataAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                Algorithm algorithm = (Algorithm) adapterView.getItemAtPosition(position);
+                Toast.makeText(getApplicationContext(), "Wybrano algorytm: " + algorithm.getName(), Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
+
+
+    }
+
+    private class MyCustomAdapter extends ArrayAdapter<Algorithm> {
+        private ArrayList<Algorithm> algorithmsList;
+
+        public MyCustomAdapter(Context context, int textviewResourceid, ArrayList<Algorithm> algorithmsList) {
+            super(context, textviewResourceid, algorithmsList);
+            this.algorithmsList = new ArrayList<Algorithm>();
+            this.algorithmsList.addAll(algorithmsList);
+
+        }
+    }
+
+    private class ViewHolder {
+        TextView code;
+        CheckBox name;
+
+    }
+
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+
 }
