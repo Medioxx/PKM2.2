928e1bbc0a39b6da3279150fb99a3c2bd3ec3395
da_wid <da_wid@onet.pl>
2017-11-29 23:11:36 +0100

    clean up

--- PKM2/ALGORYTMY/kamera_z_gory/widok_z_gory.py
+++ PKM2/ALGORYTMY/kamera_z_gory/widok_z_gory.py
@@ -3,19 +3,20 @@ import matplotlib.pyplot as plt
 from peakdetect import *
 from kmeans import *
 import os, json
-# from parser import save_to_json
 
+refresh_rate = 20 #czestotliwosc sprawdzania zajetosci torow
+delta = 20
+lines_ratio = 0.8
 
 def save_to_json(data,path):
     try:
-        print("Saving configuration...")
         with open(path + "/top_camera.json", "w") as top_camera_file:
             top_camera_file.write(json.dumps(data))
-        print("Configuration saved.")
     except OSError as err:
         print("Failed to save configuration: {0}".format(err))
 
     return True
+
 
 def group_lines(lines):
     for line in lines:
@@ -23,14 +24,13 @@ def group_lines(lines):
         grouped[int(x)] += 1
     return grouped
 
-  
-def find_rails(lines, clusters ,avg):
-    max, _ = peakdet(group_lines(lines), 20)
 
+def find_rails(lines):
+    #wyszukiwanie wiekszych skupisk linii, czyli potencjalnych torow
+    max, _ = peakdet(group_lines(lines), delta)
     max = np.asarray(max)
 
-    print(np.median(max, axis= 0))
-
+    #odfiltrowanie skupisk linii, ktore nie sa torami
     mu, clusters  = find_centers(max,2)
 
     if(clusters[0][0][1] > clusters[1][0][1]):
@@ -38,40 +38,33 @@ def find_rails(lines, clusters ,avg):
     else:
         cluster = 1
 
+    #wyswietlenie odfiltrowanych dnaych, x oznacza wykryty tor
     for i in range(clusters[cluster].__len__()):
         plt.plot(clusters[cluster][i][0],clusters[cluster][i][1], "x")
 
     plt.show()
 
-    return clusters[cluster], avg
-def find_trains(lines, clusters, avg):
-    peaks, _ = peakdet(group_lines(lines), 10)
-    new_peaks = []
+    return clusters[cluster]
+
+
+def find_trains(lines, clusters):
+    #szukamy torow
+    peaks, _ = peakdet(group_lines(lines), delta/2)
+
+    #struktury przechowujace wykryte tory
     rails = []
     trains_on_rails = {}
-
-    max = 1.00;
-    sum = 0.00
-    rail = -1
-    match = 0.00
 
     for k in range(0, clusters.__len__()):
         for i in range(0, peaks.__len__()):
             if(abs(clusters[k][0] - peaks[i][0]) < 3):
-                # peaks[i][1] = cluster[1] - peaks[i][1]
-
+                #stosunek ilosci lini w chwili obecnej do ilosci linii w chwili poczatkowej
                 diff = float(peaks[i][1]) / float(clusters[k][1])
-                sum += diff
                 rails.append(diff)
 
-                if(max > diff):
-                    max = diff
-                    rail = k + 1
-                    new_peaks.append(peaks[i])
-                break;
-
+    #wybranie tych torow na ktorych najprawdopobniej znajduje sie pociag
     for i in range(0, rails.__len__()):
-        if(rails[i] < 0.8):
+        if(rails[i] < lines_ratio):
             # print("Pociag na torze", i+1)
             # print(rails[i])
             zajety_tor = 'tor'+ str(i+1)
@@ -79,41 +72,35 @@ def find_trains(lines, clusters, avg):
 
     print(trains_on_rails)
     file_path = os.getcwd()
-    save_to_json(trains_on_rails,file_path)
 
-    # plt.plot(max, "x")
-    plt.show()
+    return save_to_json(trains_on_rails,file_path)
 
-    return
-
-def przeszkody(frame, counter, rails, clusters, avg):
-    subframe=frame
-
+def zajetosc_torow(counter, clusters):
 
     # wykrywanie lini
-    edges = cv2.Canny(subframe, 50, 250, apertureSize=3)
+    edges = cv2.Canny(frame, 50, 250, apertureSize=3)
     minLineLength = 50
     lines = cv2.HoughLinesP(image=edges, rho=1, theta=np.pi / 180, threshold=70, lines=np.array([]),
                             minLineLength=minLineLength, maxLineGap=5)
 
+    #pogrupowanie linii do odpowiednich obszarow obrazu
     group_lines(lines)
 
-    if (counter == 20):
+    if (counter == refresh_rate):
 
         if(clusters.__len__() == 0):
-            clusters, avg = find_rails(lines, clusters, avg)
+            clusters = find_rails(lines)
         else:
-            find_trains(lines, clusters, avg)
+            if(find_trains(lines, clusters) != True):
+                return
 
+        #wyzerowanie zmiennych
         counter = 0
         for element in range (0, grouped.__len__()):
             grouped[element] = 0
 
-    for element in rails:
-        cv2.line(subframe, (int(element * 12.8), 0), (int(element * 12.8), 1024), (0, 0, 255), 6,
-                  cv2.LINE_8)
 
-    return grouped, counter, rails, clusters, avg
+    return counter,clusters
 
 
 path = os.getcwd() + "\streams\\"
@@ -121,15 +108,13 @@ filename = "5.avi"
 video = cv2.VideoCapture(path + filename)
 
 grouped = [0] * 100
-rails = []
 clusters = []
-avg = 0
 counter = 0
 
-while(1):
+while(video.isOpened()):
     _, frame = video.read()
 
-    grouped, counter, rails, clusters, avg = przeszkody(frame, counter, rails, clusters, avg)
+    counter, clusters = zajetosc_torow(counter, clusters)
     counter += 1
 
     cv2.imshow('frame',frame)
